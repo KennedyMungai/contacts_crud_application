458498a4485fed135cbfcc473cebededa3327326
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 13:57:11 +0300

    Added some dummy markupo to the homepage of the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,58 @@
-import { Flex } from '@chakra-ui/react'
+import {
+	Flex,
+	Heading,
+	Table,
+	TableContainer,
+	Tbody,
+	Td,
+	Tfoot,
+	Th,
+	Thead,
+	Tr,
+	VStack
+} from '@chakra-ui/react'
 
 export default function Home() {
 	return (
 		<Flex justify={'center'} p={'4rem'}>
-			Something in the way
+			<VStack spacing={'4rem'}>
+				<Heading>Contacts</Heading>
+				<TableContainer>
+					<Table size='md'>
+						<Thead>
+							<Tr>
+								<Th>To convert</Th>
+								<Th>into</Th>
+								<Th isNumeric>multiply by</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							<Tr>
+								<Td>inches</Td>
+								<Td>millimetres (mm)</Td>
+								<Td isNumeric>25.4</Td>
+							</Tr>
+							<Tr>
+								<Td>feet</Td>
+								<Td>centimetres (cm)</Td>
+								<Td isNumeric>30.48</Td>
+							</Tr>
+							<Tr>
+								<Td>yards</Td>
+								<Td>metres (m)</Td>
+								<Td isNumeric>0.91444</Td>
+							</Tr>
+						</Tbody>
+						<Tfoot>
+							<Tr>
+								<Th>To convert</Th>
+								<Th>into</Th>
+								<Th isNumeric>multiply by</Th>
+							</Tr>
+						</Tfoot>
+					</Table>
+				</TableContainer>
+			</VStack>
 		</Flex>
 	)
 }
