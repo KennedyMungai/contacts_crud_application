d04666ce6c13530019745161642bdaf260a10968
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 14:03:14 +0300

    Got rid of the extra table rows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,16 +40,6 @@ export default function Home() {
 								<Td>millimetres (mm)</Td>
 								<Td isNumeric>25.4</Td>
 							</Tr>
-							<Tr>
-								<Td>feet</Td>
-								<Td>centimetres (cm)</Td>
-								<Td isNumeric>30.48</Td>
-							</Tr>
-							<Tr>
-								<Td>yards</Td>
-								<Td>metres (m)</Td>
-								<Td isNumeric>0.91444</Td>
-							</Tr>
 						</Tbody>
 					</Table>
 				</TableContainer>
