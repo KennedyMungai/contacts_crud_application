0820ba4c8b8bc0140a2c4bdd3729ef694869c52e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 12:23:24 +0300

    The basic setup for the project

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+	fonts: {
+		heading: 'roboto ,sans-serif',
+		body: 'montserrat ,sans-serif'
+	}
+})
+
+export default theme
