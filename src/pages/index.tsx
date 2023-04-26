501f41dd950860e454f6791acf7c0668a32c061c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 14:01:22 +0300

    Added the addCOntact button and updated the stack spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useContactsQuery } from '@/services/contactsApi'
 import {
+	Button,
 	Flex,
 	Heading,
 	Table,
@@ -12,14 +13,18 @@ import {
 	Tr,
 	VStack
 } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Home() {
 	const { data, isLoading, error } = useContactsQuery()
 
 	return (
 		<Flex justify={'center'} p={'4rem'}>
-			<VStack spacing={'4rem'}>
+			<VStack spacing={'2rem'}>
 				<Heading>Contacts</Heading>
+				<Button bg={'blue.100'}>
+					<Link href='/addEdit'>Add Contact</Link>
+				</Button>
 				<TableContainer>
 					<Table size='md'>
 						<Thead>
