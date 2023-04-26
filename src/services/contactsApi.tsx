2b47c246c2026f2db074dbe3b52c74b2f2fc6369
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 12:52:00 +0300

    Defined teh skeletion of contactsApi

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -1,10 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { IContact } from '../models/contact.model'
 
-type Props = {}
-
-const contactsApi = (props: Props) => {
-	return <div>contactsApi</div>
-}
-
-export default contactsApi
+export const contactsApi = createApi({
+    reducerPath: 'contactsApi',
+    baseQuery: fetchBaseQuery({}),
+    endpoints: (builder) => ({})
+}}
