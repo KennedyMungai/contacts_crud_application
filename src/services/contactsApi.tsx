250943e1286904d9f69fdf8d3e79b3b13fc9886f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 12:58:14 +0300

    Created the contacts endpoint for the contactsApi

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -1,8 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { IContact } from '../models/contact.model'
+
+interface IContact {
+	id?: string
+	name: string
+	email: string
+	contact: string
+}
 
 export const contactsApi = createApi({
-    reducerPath: 'contactsApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5698'}),
-    endpoints: (builder) => ({})
-}}
+	reducerPath: 'contactsApi',
+	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5698' }),
+	endpoints: (builder) => ({
+		contacts: builder.query<IContact[], void>({
+			query: () => '/contacts'
+		})
+	})
+})
+
+export const { useContactsQuery } = contactsApi
