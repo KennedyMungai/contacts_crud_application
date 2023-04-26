c26bf2b85a84a4eec00725362ba8a48f64c4cb01
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-26 13:05:15 +0300

    Added the logic

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,12 @@
 import { contactsApi } from '@/services/contactsApi'
 import { configureStore } from '@reduxjs/toolkit'
 
-const store = configureStore({})
+const store = configureStore({
+	reducer: {
+		[contactsApi.reducerPath]: contactsApi.reducer
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(contactsApi.middleware)
+})
+
+export default store
